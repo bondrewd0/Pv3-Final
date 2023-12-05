8bf88652688ea3e2421ae5e73f9441baa460557f
bondrewd0 <agustinchan@hotmail.com.ar>
2023-12-05 10:10:26 -0300

    movement first trys

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,4 +1,4 @@
-tool
+
 extends RigidBody2D
 #Collision type
 var type='Enemy'
@@ -9,20 +9,29 @@ onready var changeState=$StateChanger
 onready var fireRef=$Fire
 onready var playerRef=get_parent().get_node('Player')
 onready var targetRef=$TargetDir
+onready var tween=$Movement
 var bulletScene=preload("res://Scenes/Bullet.tscn")
 
 #Variables for enemy type:
 var attackType=0
 export var fireRate =0
 var state
-
+var finalPos=Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	state=0
 	targetRef.position=Vector2(0,-50)
 	fireRef.start(fireRate)
-
+	if attackType==1:
+		finalPos=Vector2(self.position.x,self.position.y+900)
+		tween.interpolate_property(self,'position',position,finalPos,9,Tween.TRANS_CIRC,Tween.EASE_IN,0.3)
+	if attackType==0:
+		finalPos=Vector2(0,self.position.y+700)
+		print(finalPos.x)
+		print(self.position.x)
+		tween.interpolate_property(self,'position',position,finalPos,9,Tween.TRANS_CIRC,Tween.EASE_IN,0.3)
+	tween.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
