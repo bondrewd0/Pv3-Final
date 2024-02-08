af842220ed8f13c600662da8d8059f3cf8765a87
bondrewd0 <agustinchan@hotmail.com.ar>
2024-02-08 20:37:10 -0300

    Balance and sound ajustments

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 var settingsRef=preload("res://Scenes/Settings.tscn")
 func _on_Play_pressed():
-	get_tree().change_scene("res://Scenes/Nivel 1.tscn")
+	get_tree().change_scene("res://Scenes/Nivel2.tscn")
 
 
 func _on_Exit_pressed():
